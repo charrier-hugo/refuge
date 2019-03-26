b82663f0da82a5f9696fdcf060717fc6cd941abb
std <std@infogestion.local>
2019-03-26 16:47:22 +0100

    first commit

---
+++ recherche.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href="CSS/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="CSS/custom.css" rel="stylesheet" type="text/css">
+	<title>Home</title>
+</head>
+<header>
+	<div class="row">
+		<div class="offset-1 col-10">
+			<img id="img_header" class="mx-auto d-block" src="image/header.png">
+		</div>
+	</div>
+</header>
+<body>
+	<div class="container">
+		<div class="row border">
+				<nav class="navbar navbar-inverse nav-pills nav-fill col-12">
+			    	<button class="btn btn-link"><a class="nav-item nav-link" href="index.html">Acceuil</a></button>
+			    	<button class="btn btn-link"><a class="nav-item nav-link active" href="recherche.php">Recherche</a></button>
+			    	<button class="btn btn-link"><a class="nav-item nav-link" href="#">Contact</a></button>
+				</nav>
+	   	</div>
+		<br>	   
+		
+
+	</div>
+	
+</body>
+</html>
